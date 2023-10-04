840d48a4a6d1068127006650d9d2d76c1b3ccca0
billythedummy <dhy1996@live.com.sg>
2023-10-04 11:12:50 +0800

    base64 -> data-encoding

--- tower-etag-cache/src/base64_blake3_body_etag.rs
+++ tower-etag-cache/src/base64_blake3_body_etag.rs
@@ -1,4 +1,4 @@
-use base64::{engine::general_purpose, Engine};
+use data_encoding::BASE64;
 use http::HeaderValue;
 
 /// Calculates the etag value as base64 encoded blake3 hash of the body bytes
@@ -6,7 +6,7 @@ pub fn base64_blake3_body_etag(body: impl AsRef<[u8]>) -> HeaderValue {
     let mut hasher = blake3::Hasher::new();
     hasher.update(body.as_ref());
     let bytes = hasher.finalize();
-    let val = general_purpose::STANDARD.encode(bytes.as_bytes());
+    let val = BASE64.encode(bytes.as_bytes());
     // base64 should be always valid ascii
     HeaderValue::from_str(&val).unwrap()
 }
